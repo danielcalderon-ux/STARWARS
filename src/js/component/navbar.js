3cb0df347332f1fe5b4290a226de82c46e7f04d0
danielcalderon-ux <danielcal89@hotmail.com>
2021-03-01 22:27:32 +0000

    favorito

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,37 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, setState, useContext } from "react";
 import { Link } from "react-router-dom";
 import rigoImage from "../../img/sw.jpg";
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggle = () => setDropdownOpen(prevState => !prevState);
 	return (
 		<nav className="navbar navbar-light bg-secondary mb-3">
 			<Link to="/">
 				<i className="fab fa-jedi-order fa-3x" />
 				<img src={rigoImage} height="80" />
 			</Link>
-			<div className="ml-auto">
-				<div className="btn-group" role="group">
-					<button
-						id="btnGroupDrop1"
-						type="button"
-						className="btn btn-primary dropdown-toggle"
-						data-bs-toggle="dropdown"
-						aria-expanded="false">
-						Favoritos
-					</button>
-					<ul className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-						<li>
-							<a className="dropdown-item" href="#">
-								Dropdown link
-							</a>
-						</li>
-						<li>
-							<a className="dropdown-item" href="#">
-								Dropdown link
-							</a>
-						</li>
-					</ul>
-				</div>
+			<div className="ml-auto,">
+				<Dropdown isOpen={dropdownOpen} toggle={toggle} className="text-primary" color="primary">
+					<DropdownToggle caret>Favoritos</DropdownToggle>
+					<DropdownMenu>
+						<DropdownItem header>PERSONAJES</DropdownItem>
+						{store.favorites.map((favorite, i) => {
+							if (favorite.type == "persona") {
+								return (
+									<div key={i}>
+										<DropdownItem text className="text-primary">
+											{favorite.name}
+										</DropdownItem>
+									</div>
+								);
+							}
+						})}
+
+						<DropdownItem divider />
+						<DropdownItem header>PLANETAS</DropdownItem>
+						{store.favorites.map((favorite, i) => {
+							if (favorite.type == "planeta") {
+								return (
+									<div key={i}>
+										<DropdownItem text className="text-primary">
+											{favorite.name}
+										</DropdownItem>
+									</div>
+								);
+							}
+						})}
+					</DropdownMenu>
+				</Dropdown>
 			</div>
 		</nav>
 	);
--- src/js/component/planetas.jsx
+++ src/js/component/planetas.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, setState, useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { Link } from "react-router-dom";
 import propTypes from "prop-types";
+import { Context } from "../store/appContext";
 
-export const Planetas = props => (
-	<div className="card mx-2" style={{ width: "18rem" }}>
-		<img
-			src="https://lafuerzanoticias.files.wordpress.com/2018/10/mustafar-tall.jpg?w=1536&h=768&crop=1"
-			className="card-img-top"
-			alt="..."
-		/>
-		<div className="card-body">
-			<h5 className="card-title">{props.nombre}</h5>
-			<p className="card-text">
-				Some quick example text to build on the card title and make up the bulk of the cards content.
-			</p>
-			<Link to={`/planetas/${props.id}`} className="btn btn-primary">
-				Go somewhere
-			</Link>
-			<button type="button" className="btn btn-outline-info float-right">
-				<i className="far fa-heart text-danger " />
-			</button>
+export const Planetas = props => {
+	const { store, actions } = useContext(Context);
+	const nombre = props.nombre;
+
+	return (
+		<div className="card mx-2" style={{ width: "18rem" }}>
+			<img
+				src="https://lafuerzanoticias.files.wordpress.com/2018/10/mustafar-tall.jpg?w=1536&h=768&crop=1"
+				className="card-img-top"
+				alt="..."
+			/>
+			<div className="card-body">
+				<h5 className="card-title">{props.nombre}</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make up the bulk of the cards content.
+				</p>
+				<Link to={`/planetas/${props.id}`} className="btn btn-primary">
+					Go somewhere
+				</Link>
+				<Link onClick={() => actions.addFavorite(nombre, "planeta")}>
+					<button type="button" className="btn btn-outline-info float-right">
+						<i className="far fa-heart text-danger " />
+					</button>
+				</Link>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 Planetas.propTypes = {
 	nombre: propTypes.string,
